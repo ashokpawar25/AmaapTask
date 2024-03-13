dcc5678fc7e615e2207af460a18bfc9d5c90fc3c
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-03-13 23:28:58 +0530

    User All validations done

--- unusualSpend/src/test/java/com/ttp/user/UserTest.java
+++ unusualSpend/src/test/java/com/ttp/user/UserTest.java
@@ -2,6 +2,7 @@ package com.ttp.user;
 
 import com.ttp.invalidEmailException.InvalideEmailException;
 import com.ttp.User;
+import com.ttp.invalidUserNameException.InvalideUserNameException;
 import com.ttp.invalideUserException.InvalideUserIdException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,12 @@ public class UserTest {
     void shouldthrowExeceptionWhenUserEmailIsInvalid() throws InvalideUserIdException {
         Assertions.assertThrows(InvalideEmailException.class, () -> User.create(3, "Ashok Pawar", null));
         Assertions.assertThrows(InvalideEmailException.class, () -> User.create(4, "Ashok Pawar", ""));
-        Assertions.assertDoesNotThrow(()-> User.create(2,"Ashok Pawar","ashokpawar8020@gmail.com"));
+        Assertions.assertDoesNotThrow(() -> User.create(2, "Ashok Pawar", "ashokpawar8020@gmail.com"));
+    }
+
+    @Test
+    void shouldThrowExceptioinWhenUserNameIsInvalid() throws InvalideEmailException, InvalideUserNameException, InvalideUserIdException {
+        Assertions.assertThrows(InvalideUserNameException.class, () -> User.create(4, "av al", "sample@gmail.com"));
+        Assertions.assertDoesNotThrow(() -> User.create(3, "sample Name", "sample@gmail.com"));
     }
 }

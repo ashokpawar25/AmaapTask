9ad741c439662b8e3b7154f2dc0de66064634a8f
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-03-17 14:50:17 +0530

    added method to add new transaction

--- unusualSpend/src/main/java/com/ttp/creditCardManager/CreditCardManager.java
+++ unusualSpend/src/main/java/com/ttp/creditCardManager/CreditCardManager.java
@@ -1,12 +1,32 @@
 package com.ttp.creditCardManager;
 
+import com.ttp.Transaction;
 import com.ttp.User;
 import com.ttp.creditCard.CreditCard;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreditCardManager {
+
+    List<Transaction> transactions = new ArrayList<>();
     public boolean mapCardToUser(CreditCard creditCard, User user)
     {
         creditCard.setUser(user);
         return true;
+    }
+
+    public void addTransaction(List<Transaction> transactions)
+    {
+        for(Transaction transaction:transactions)
+        {
+            this.transactions.add(transaction);
+        }
+
+    }
+
+    public List<Transaction> getTransactions()
+    {
+        return this.transactions;
     }
 }
--- unusualSpend/src/test/java/com/ttp/creditCardManager/CreditCardManagerTest.java
+++ unusualSpend/src/test/java/com/ttp/creditCardManager/CreditCardManagerTest.java
@@ -1,13 +1,23 @@
 package com.ttp.creditCardManager;
 
+import com.ttp.Category.Category;
 import com.ttp.InvalidCardIdException.InvalidCardIdException;
+import com.ttp.Transaction;
 import com.ttp.User;
 import com.ttp.creditCard.CreditCard;
+import com.ttp.invalidAmountException.InvalidAmountException;
 import com.ttp.invalidEmailException.InvalideEmailException;
 import com.ttp.invalidUserNameException.InvalideUserNameException;
+import com.ttp.invalideCategoryException.InvalideCategoryException;
+import com.ttp.invalideTransactionIdException.InvalideTransactionIdException;
 import com.ttp.invalideUserException.InvalideUserIdException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CreditCardManagerTest {
     @Test
@@ -29,7 +39,7 @@ public class CreditCardManagerTest {
     }
 
     @Test
-    void shouldAbleToDoTransactionForCreditCard() throws InvalidCardIdException, InvalideEmailException, InvalideUserNameException, InvalideUserIdException {
+    void shouldAbleToAddTransactionForCreditCard() throws InvalidCardIdException, InvalideEmailException, InvalideUserNameException, InvalideUserIdException, InvalideTransactionIdException, InvalideCategoryException, InvalidAmountException {
         //Arrange
         CreditCard creditCard = CreditCard.create(1);
 
@@ -42,6 +52,40 @@ public class CreditCardManagerTest {
         CreditCardManager creditCardManager = new CreditCardManager();
         creditCardManager.mapCardToUser(creditCard, user);
 
-//        creditCardManager.
+        Transaction transaction = Transaction.create(101, Category.travel, 100, LocalDate.of(2024, 03, 17), 1);
+        List<Transaction> expectedTransactionList = List.of(transaction);
+        creditCardManager.addTransaction(expectedTransactionList);
+
+        List<Transaction> transactions = creditCardManager.getTransactions();
+
+        //Assert
+        Assertions.assertEquals(transaction, transactions.get(0));
     }
+
+    @Test
+    void shouldAbleToAddMultipleTransactionsForCreditCard() throws InvalideTransactionIdException, InvalideCategoryException, InvalidAmountException, InvalideEmailException, InvalideUserNameException, InvalideUserIdException, InvalidCardIdException {
+        //Arrange
+        CreditCard creditCard = CreditCard.create(1);
+
+        int userID = 1;
+        String userName = "Ashok Pawar";
+        String userEmail = "ashokpawar25052001@gmail.com";
+        User user = User.create(userID, userName, userEmail);
+
+        //Act
+        CreditCardManager creditCardManager = new CreditCardManager();
+        creditCardManager.mapCardToUser(creditCard, user);
+
+        Transaction transaction1 = Transaction.create(101, Category.travel, 100, LocalDate.of(2024, 03, 17), 1);
+        Transaction transaction2 = Transaction.create(101, Category.travel, 100, LocalDate.of(2024, 03, 17), 1);
+        List<Transaction> expectedTransactionList = List.of(transaction1, transaction2);
+
+        creditCardManager.addTransaction(expectedTransactionList);
+
+        List<Transaction> transactions = creditCardManager.getTransactions();
+
+        //Assert
+        Assertions.assertEquals(2, transactions.size());
+    }
+
 }

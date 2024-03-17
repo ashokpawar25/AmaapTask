9cd1d477ca93be134a566d3d76a2f788385457aa
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-03-17 21:05:35 +0530

    Unwanted files deleted

--- unusualSpend/src/main/java/com/ttp/UserHandler.java
+++ unusualSpend/src/main/java/com/ttp/UserHandler.java
@@ -17,25 +17,5 @@ public class UserHandler
         this.users.add(user);
     }
 
-    public User getUserByUserId(int userId1)
-    {
-//        User user = new User(1,"sameer","ashokpawar25052001@gmail.com");
-//        return user;
-        for(User user1:users)
-        {
-            if (user1.userId==userId1)
-            {
-                return user1;
-            }
-        }
-        return null;
-    }
 
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
 }
